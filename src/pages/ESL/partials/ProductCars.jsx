22b69375f9d0a216d907bc15ee4d523ac61dd035
shahbazasghar1038 <muhammadshahbaz1038@gmail.com>
2025-02-03 23:35:20 +0500

    fixing

--- src/pages/ESL/partials/ProductCars.jsx
+++ src/pages/ESL/partials/ProductCars.jsx
@@ -4,41 +4,25 @@ import ProductCards3 from "../../../assets/ProductCards3.png";
 import ProductCards4 from "../../../assets/ProductCards4.png";
 const ProductCards = () => {
   const products = [
-    {
-      id: 1,
-      name: "Multicolor 13.3 inch",
-      image: ProductCards1,
-    },
-    {
-      id: 2,
-      name: "Bluetooth E Pricer Label",
-      image: ProductCards2,
-    },
-    {
-      id: 3,
-      name: "Stretched LCD screen",
-      image: ProductCards3,
-    },
-    {
-      id: 4,
-      name: "E-INK BLE 5.0",
-      image: ProductCards4,
-    },
+    { id: 1, name: "Multicolor 13.3 inch", image: ProductCards1 },
+    { id: 2, name: "Bluetooth E Pricer Label", image: ProductCards2 },
+    { id: 3, name: "Stretched LCD screen", image: ProductCards3 },
+    { id: 4, name: "E-INK BLE 5.0", image: ProductCards4 },
   ];
 
   return (
-    <div className="max-w-[1328px] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 px-4 pb-10 ">
+    <div className="max-w-[1028px] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 px-4 pb-10 auto-rows-fr">
       {products.map((product) => (
         <div
           key={product.id}
-          className="bg-[#1D1D1F] rounded-[30px] p-6 hover:bg-gray-900 hover:text-white transition duration-300 shadow-lg flex flex-col items-center text-center"
+          className="bg-[#1D1D1F] rounded-[30px] max-w-70% p-6 hover:bg-gray-900 hover:text-white transition duration-300 shadow-lg flex flex-col items-center text-center h-full"
         >
           <h4 className="text-lg mb-3 font-semibold text-white">
             {product.name}
           </h4>
           <img
             src={product.image}
-            className="w-[60%] h-auto rounded-lg"
+            className="w-[60%] h-[180px] object-contain rounded-lg"
             alt={product.name}
           />
         </div>
