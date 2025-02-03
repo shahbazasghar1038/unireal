41d745f61318c1e48aa2e0ff2846eb5e418935b9
shahbazasghar1038 <muhammadshahbaz1038@gmail.com>
2025-02-03 19:00:17 +0500

    resolve conflict

--- src/pages/ESL/partials/ECLSlider.jsx
+++ src/pages/ESL/partials/ECLSlider.jsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
-=======
-import { useState } from "react";
->>>>>>> 1522adea6b901f862d91b2e29d4a1d974a37864a
 import slide1 from "../../../assets/slide1.png";
 
 const ECLSlider = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const slides = [
-<<<<<<< HEAD
     {
       id: 1,
       imageUrl: slide1,
@@ -34,12 +29,6 @@ const ECLSlider = () => {
       heading: "DIGITAL SIGNAGE",
       text: "Engage customers with dynamic promotions",
     },
-=======
-    { id: 1, imageUrl: slide1,content:'Fresh Food',subContent:'' },
-    { id: 2, imageUrl: slide1,content:'Retail',subContent:'' },
-    { id: 3, imageUrl: slide1,content:'Pharmacy',subContent:'' },
-    { id: 4, imageUrl: slide1,content:'Digital Signage',subContent:'' },
->>>>>>> 1522adea6b901f862d91b2e29d4a1d974a37864a
   ];
 
   const handleSlideClick = (index) => {
@@ -53,7 +42,6 @@ const ECLSlider = () => {
           key={slide.id}
           className={`slide ${activeIndex === index ? "active" : ""}`}
           onClick={() => handleSlideClick(index)}
-<<<<<<< HEAD
           style={{
             backgroundImage: `url(${slide.imageUrl})`,
           }}
@@ -62,12 +50,6 @@ const ECLSlider = () => {
             <h2 className="slide-heading">{slide.heading}</h2>
             <p className="slide-text">{slide.text}</p>
           </div>
-=======
-          style={{ backgroundImage: `url(${slide.imageUrl})` }}
-        >
-          <p>{slide.content}<br/></p>
-          <span>{slide.subContent}</span>
->>>>>>> 1522adea6b901f862d91b2e29d4a1d974a37864a
         </div>
       ))}
     </div>
