1522adea6b901f862d91b2e29d4a1d974a37864a
shumailrafique-info <shumailrafique.info@gmail.com>
2025-02-03 15:03:13 +0500

    code is similar now

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import ScrollingText from "./scrolling-text";
 
 const HeroSection = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleCanPlay = () => {
+    setIsLoading(false); // Video is ready to play
+  };
+
   return (
     <>
       <ScrollingText />
@@ -62,26 +69,22 @@ const HeroSection = () => {
           <img src="/unireal.png" className="w-full" alt="" />
         </div>
         <div className="relative flex w-full items-center justify-center pt-[60px] sm:pt-[110px] xl:pt-[158px] 2xl:pt-[158px]">
-          {/* <img
-            src="/bg-her-gradient.webp"
-            alt=""
-            className="pointer-events-none absolute top-[10%] z-[-1] w-full scale-[1.6] animate-[gradientFade_6s_ease-in-out_infinite] sm:top-[-1%] sm:scale-[1] md:top-[-3%] lg:top-[-80px] 2xl:top-[-130px]"
-          /> */}
           <div className="gradient-hover-effect-background relative mx-auto flex w-full max-w-[90%] items-center justify-center px-[8px] py-[4px] sm:max-w-[77%] xl:px-[14px] xl:py-[7px]">
             <div className="absolute top-0 z-[-1] h-[250px] w-full bg-custom-gradient blur-[6px] md:h-[600px] md:blur-[15px] xl:blur-[16px] 2xl:h-[578px] 2xl:blur-[16px]"></div>
-            <div className="relative aspect-[2/1.3] w-full overflow-hidden rounded-[5px] bg-[black] p-4 sm:aspect-[2/1]">
-              {/* <img
-              src="/her-vide-img.png"
-              className="h-full w-full rounded-[5px] object-cover"
-              alt=""
-            /> */}
-              <div className="h-full w-full bg-gray-800">
+            <div className="relative aspect-[2/1.3] w-full overflow-hidden rounded-[5px] bg-[black] sm:aspect-[2/1]">
+              <div className="relative h-full w-full !border-[16px] !border-[black] bg-gray-800">
+                {isLoading && (
+                  <div className="flex h-full w-full animate-pulse items-center justify-center bg-black text-sm">
+                    Loading...
+                  </div>
+                )}
                 <video
                   autoPlay
                   loop
                   muted
                   playsInline
                   controls={false}
+                  onCanPlay={handleCanPlay}
                   className="h-full w-full object-cover"
                 >
                   <source src="/V2.mp4" type="video/mp4" />
